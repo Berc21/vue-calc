8e0910f7568126888daeefdd8bab095e3f3d1937
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-07-05 12:40:21 +0300

    init, add calculator component and install sass

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="calculator">
+    <ul class="calculator-list">
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+      <li>Content</li>
+    </ul>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
